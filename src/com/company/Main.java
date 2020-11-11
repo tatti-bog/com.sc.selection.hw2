0303a7f3b1179ca9cf91af3897dfc1df4557711f
Tania <tanya6785412@gmail.com>
2020-11-12 00:35:39 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+import static ibio.Helpers.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String [] students = { "Donald", "Evgeney", "Charlie", "Khalid"};
+        String [] grades = { "B", "B", "A", "C"};
+        String search = input("who's grade do you want to know? Enter the name: ");
+        int index = 0;
+        for (int i=0; i < students.length; i++)
+        {
+            if (students[i].equals(search))
+            {
+                index=i;
+                System.out.println(grades[i]);
+            }
+        }
+
+
+    }
+
+}
